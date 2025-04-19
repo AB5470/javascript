386206a7dc850501e285edf7cf3d24ece62766b5
Aayush Bhardwaj <155520876+AB5470@users.noreply.github.com>
2025-04-19 06:53:17 +0000

    basics done

---
+++ basics/first.js
@@ -0,0 +1,17 @@
+console.log("Hellow Aayush")
+const clg = "galgotias"
+
+let age = 22
+
+var course = "btech" // we should not use var keyword because it will not consider block scope
+
+name = "Aayush" // we can use a variable without any keyword i.e const, let or var.., but this is not a right way so we should ignore it..
+
+//clg = "glbajaj" // cant change any variable with const keyword
+age = 23
+course = "BBA"
+console.table([age,course,clg,name])
+
+
+
+//conclusion only use cont and let keywords in javascript
