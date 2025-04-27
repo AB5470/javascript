b5fcb9026bd2c77634adc703a114fa3bee420b7a
Aayush Bhardwaj <155520876+AB5470@users.noreply.github.com>
2025-04-27 06:56:45 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,18 @@
+ age = 18   //number
+ fname = "Aayush"  //string
+ course = null  //object
+ 
+
+
+
+ console.log(typeof(age));
+ console.log(typeof(fname));
+ console.log(typeof(course));
+ console.log(typeof undefined);
+
+
+ // NOTE the type of null is object while the type of undefined is undefined itself
+ 
+
+
+ 
